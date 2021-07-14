06d3ae639c502ec88d164349682f6d05d7f22fda
huayunliufeng <3300358443@qq.com>
2021-07-15 01:30:51 +0800

    第二次提交

---
+++ src/main/java/com/zhong/service/Service.java
@@ -0,0 +1,11 @@
+package com.zhong.service;
+
+/**
+ * @author 华韵流风
+ * @ClassName Service
+ * @Description TODO
+ * @Date 2021/7/15 1:21
+ * @packageName com.zhong.service
+ */
+public interface Service {
+}
---
+++ src/main/java/com/zhong/web/Controller.java
@@ -0,0 +1,13 @@
+package com.zhong.web;
+
+/**
+ * @author 华韵流风
+ * @ClassName Controller
+ * @Description TODO
+ * @Date 2021/7/15 1:19
+ * @packageName com.zhong.web
+ */
+public class Controller {
+
+
+}
